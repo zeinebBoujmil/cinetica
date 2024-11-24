e49c56c35ee6ec0565ae0d7c18b87a65b29ab227
zeinebBoujmil <zeinebboujmil253@gmail.com>
2024-11-24 20:57:09 +0100

    mm

--- app/dashboard/Favorites/FavoriteShows/page.tsx
+++ app/dashboard/Favorites/FavoriteShows/page.tsx
@@ -4,46 +4,38 @@ import { TVShow } from '@/app/entities/TVShow';
 import ShowCard from '../../cards/showCard';
 import { useSearch } from '../../contexts/searchContext';
 
-interface FavoritesSeriesProps {
-  onSerieClick: (serie: TVShow) => void;
-}
-
-const FavoritesSeries: React.FC<FavoritesSeriesProps> = ({ onSerieClick }) => {
-  const [FavoritesSeries, setFavorites] = useState<TVShow[]>([]);
+const FavoritesSeries = ({ onSerieClick }: { onSerieClick: (serie: TVShow) => void }) => {
+  const [favoritesSeries, setFavorites] = useState<TVShow[]>([]);
   const { query } = useSearch();
   
   useEffect(() => {
-    // Vérifier si nous sommes dans un environnement client avant d'utiliser localStorage
     if (typeof window !== "undefined") {
       const currentUser = localStorage.getItem('currentUser'); 
       if (currentUser) {
         const storedFavorites = JSON.parse(localStorage.getItem(`favoritesSeries_${currentUser}`) || '[]');
         
-        // Appliquer un filtre si `searchQuery` est défini
         if (query) {
           const filteredFavorites = storedFavorites.filter((show: TVShow) =>
             show.name.toLowerCase().includes(query.toLowerCase())
           );
           setFavorites(filteredFavorites);
         } else {
-          setFavorites(storedFavorites); // Si pas de `searchQuery`, afficher tous les favoris
+          setFavorites(storedFavorites);
         }
       }
     }
-  }, [query]); // Déclencher l'effet à chaque changement de `searchQuery`
+  }, [query]);
 
   return (
     <div className="min-h-screen text-gray-700 py-8 px-6">
       <h2 className="text-4xl font-bold mb-6 text-center">Vos Séries Favorites</h2>
-      {FavoritesSeries.length > 0 ? (
+      {favoritesSeries.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {FavoritesSeries.map((serie) => (
+          {favoritesSeries.map((serie) => (
             <div key={serie.id} className="flex justify-center">
               <ShowCard
                 serie={serie}
-                onClick={() => {
-                  onSerieClick(serie);
-                }}
+                onClick={() => onSerieClick(serie)}
               />
             </div>
           ))}
